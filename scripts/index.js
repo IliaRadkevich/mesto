3b0f60efdd2f223d247d9866ca9a4570a270c1e1
Ilia Radkevich <Kisamoriska@yandex.ru>
2022-05-03 20:01:49 +0500

    Fix : исправлены все замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,30 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = document.querySelector('.popup__close');
+const popupCloseButton = document.querySelector('.popup_closed');
 const popup = document.querySelector('.popup');
+const formElement = document.querySelector('.popup__form');
+let job = document.querySelector('.popup__input-occupation');
+let occupation = document.querySelector('.profile__occupation');
+let name = document.querySelector('.popup__input-name');
+let title = document.querySelector('.profile__title');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-occupation');
 
 function openedPopup() {
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
+  name.value=title.textContent
+  job.value=occupation.textContent
 }
 function closedPopup() {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 }
 
 popupOpenButton.addEventListener('click', openedPopup);
 popupCloseButton.addEventListener('click', closedPopup);
 
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-occupation');
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let name = document.querySelector('.popup__input-name').value;
-  let job = document.querySelector('.popup__input-occupation').value;
-  let title = document.querySelector('.profile__title');
-  title.textContent = name;
-  let occupation = document.querySelector('.profile__occupation');
-  occupation.textContent = job;
+  title.textContent = name.value;
+  occupation.textContent = job.value;
   closedPopup();
 }
 
